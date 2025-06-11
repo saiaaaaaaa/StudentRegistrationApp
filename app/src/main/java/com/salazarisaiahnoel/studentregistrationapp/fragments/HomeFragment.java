66a4da29435dd0ed949462fbf230d72f44fd3e15
saiasasaia <isaiahnoelsalazar474@gmail.com>
2025-06-11 21:38:37 +0800

    Validation added

--- app/src/main/java/com/salazarisaiahnoel/studentregistrationapp/fragments/HomeFragment.java
+++ app/src/main/java/com/salazarisaiahnoel/studentregistrationapp/fragments/HomeFragment.java
@@ -9,6 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
@@ -23,9 +24,14 @@ import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.saiaaaaaaa.mywebsite_androiddependency.Check;
 import com.github.saiaaaaaaa.mywebsite_androiddependency.EasySQL;
 import com.github.saiaaaaaaa.mywebsite_androiddependency.RoundedAlertDialog;
 import com.salazarisaiahnoel.studentregistrationapp.R;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
@@ -33,7 +39,7 @@ public class HomeFragment extends Fragment {
     ImageView studentIdSelector, bdaySelector;
     RadioButton rbMale, rbFemale;
     Button btnRegister, btnUpdate, btnDelete;
-    
+    EasySQL easySQL;
     final String db = "sra_db";
     final String table = "sra_table";
     final String[] columns = {"student_id:text", "last_name:text", "first_name:text", "middle_name:text", "birthday:text", "gender:text"};
@@ -66,7 +72,7 @@ public class HomeFragment extends Fragment {
         btnUpdate = view.findViewById(R.id.btn_update);
         btnDelete = view.findViewById(R.id.btn_delete);
 
-        EasySQL easySQL = new EasySQL(requireContext());
+        easySQL = new EasySQL(requireContext());
         if (!easySQL.doesTableExist(db, table)){
             easySQL.createTable(db, table, columns);
         }
@@ -86,20 +92,33 @@ public class HomeFragment extends Fragment {
 
                 RoundedAlertDialog rad = new RoundedAlertDialog(requireContext());
 
+                List<StringArrayObject> ids = new ArrayList<>();
+
                 for (String[] a : easySQL.getTableValuesAsArray(db, table)){
+                    ids.add(new StringArrayObject(a, a[0].split(":")[1].replace("'", "")));
+                }
+
+                ids.sort(new Comparator<StringArrayObject>() {
+                    @Override
+                    public int compare(StringArrayObject o1, StringArrayObject o2) {
+                        return o1.name.compareTo(o2.name);
+                    }
+                });
+
+                for (StringArrayObject sao : ids){
                     TextView tv = new TextView(requireContext());
-                    tv.setText(a[0].split(":")[1].replace("'", ""));
+                    tv.setText(sao.array[0].split(":")[1].replace("'", ""));
                     tv.setTextColor(requireContext().getColor(R.color.black));
                     tv.setBackgroundResource(drawableResId);
                     tv.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            studentId.setText(a[0].split(":")[1].replace("'", ""));
-                            lastName.setText(a[1].split(":")[1].replace("'", ""));
-                            firstName.setText(a[2].split(":")[1].replace("'", ""));
-                            middleName.setText(a[3].split(":")[1].replace("'", ""));
-                            birthday.setText(a[4].split(":")[1].replace("'", ""));
-                            if (a[5].split(":")[1].replace("'", "").equals("Male")){
+                            studentId.setText(sao.array[0].split(":")[1].replace("'", ""));
+                            lastName.setText(sao.array[1].split(":")[1].replace("'", ""));
+                            firstName.setText(sao.array[2].split(":")[1].replace("'", ""));
+                            middleName.setText(sao.array[3].split(":")[1].replace("'", ""));
+                            birthday.setText(sao.array[4].split(":")[1].replace("'", ""));
+                            if (sao.array[5].split(":")[1].replace("'", "").equals("Male")){
                                 rbMale.setChecked(true);
                                 rbFemale.setChecked(false);
                             } else {
@@ -137,26 +156,61 @@ public class HomeFragment extends Fragment {
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                easySQL.insertToTable(db, table, new String[]{"student_id:" + studentId.getText().toString(), "last_name:" + lastName.getText().toString(), "first_name:" + firstName.getText().toString(), "middle_name:" + middleName.getText().toString(), "birthday:" + birthday.getText().toString(), "gender:" + (rbMale.isChecked() ? "Male" : "Female")});
-                Toast.makeText(requireContext(), "Register success.", Toast.LENGTH_SHORT).show();
+                if ((!Check.hasSymbols(studentId.getText().toString()) && !Check.hasSpaces(studentId.getText().toString())) && validName(lastName.getText().toString()) && validName(firstName.getText().toString()) && validName(middleName.getText().toString()) && !birthday.getText().toString().equals(" / /") && (rbMale.isChecked() || rbFemale.isChecked())){
+                    easySQL.insertToTable(db, table, new String[]{"student_id:" + studentId.getText().toString(), "last_name:" + lastName.getText().toString(), "first_name:" + firstName.getText().toString(), "middle_name:" + middleName.getText().toString(), "birthday:" + birthday.getText().toString(), "gender:" + (rbMale.isChecked() ? "Male" : "Female")});
+                    Toast.makeText(requireContext(), "Register success.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(requireContext(), "Please check your inputs.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
         btnUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                easySQL.deleteFromTable(db, table, "student_id:" + studentId.getText().toString());
-                easySQL.insertToTable(db, table, new String[]{"student_id:" + studentId.getText().toString(), "last_name:" + lastName.getText().toString(), "first_name:" + firstName.getText().toString(), "middle_name:" + middleName.getText().toString(), "birthday:" + birthday.getText().toString(), "gender:" + (rbMale.isChecked() ? "Male" : "Female")});
-                Toast.makeText(requireContext(), "Update success.", Toast.LENGTH_SHORT).show();
+                if ((!Check.hasSymbols(studentId.getText().toString()) && !Check.hasSpaces(studentId.getText().toString())) && validName(lastName.getText().toString()) && validName(firstName.getText().toString()) && validName(middleName.getText().toString()) && !birthday.getText().toString().equals(" / /") && (rbMale.isChecked() || rbFemale.isChecked())){
+                    easySQL.deleteFromTable(db, table, "student_id:" + studentId.getText().toString());
+                    easySQL.insertToTable(db, table, new String[]{"student_id:" + studentId.getText().toString(), "last_name:" + lastName.getText().toString(), "first_name:" + firstName.getText().toString(), "middle_name:" + middleName.getText().toString(), "birthday:" + birthday.getText().toString(), "gender:" + (rbMale.isChecked() ? "Male" : "Female")});
+                    Toast.makeText(requireContext(), "Update success.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(requireContext(), "Please check your inputs.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
         btnDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                easySQL.deleteFromTable(db, table, "student_id:" + studentId.getText().toString());
-                Toast.makeText(requireContext(), "Delete success.", Toast.LENGTH_SHORT).show();
+                if (validStudentID(studentId.getText().toString())){
+                    easySQL.deleteFromTable(db, table, "student_id:" + studentId.getText().toString());
+                    Toast.makeText(requireContext(), "Delete success.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(requireContext(), "Please check your student ID.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+    }
+
+    class StringArrayObject {
+        String[] array;
+        String name;
+
+        public StringArrayObject(String[] array, String name){
+            this.array = array;
+            this.name = name;
+        }
+    }
+
+    boolean validStudentID(String str){
+        for (String[] a : easySQL.getTableValuesAsArray(db, table)){
+            if (a[0].split(":")[1].replace("'", "").equals(str)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    boolean validName(String str){
+        return !Check.hasNumbers(str) && !Check.hasSymbols(str) && !TextUtils.isEmpty(str);
     }
 }
